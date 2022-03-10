8d6d36f156d2d00dd19c55d630342cf5c054be69
"amansahu278" <"a.man.cubes24@gmail.com">
2022-03-10 11:22:04 +0530

    importing owner.sol

--- contracts/modifier.sol
+++ contracts/modifier.sol
@@ -1,17 +1,6 @@
 pragma solidity ^0.8;
 
-contract Owned {
-    address owner;
-
-    constructor() {
-        owner = msg.sender;
-    }
-
-    modifier onlyOwner() {
-        require(msg.sender == owner, "Not authorized!");
-        _; // The body of the function adding the modifier onlyOwner, will be replaced in the "_"
-    }
-}
+import "./owner.sol";
 
 contract InheritanceandModifiers is Owned {
 
